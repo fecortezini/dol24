fbe344f8d178db44a6ef55abe8f68a4bd16a4a3c
felipe cortezini <felipemasterx21@gmail.com>
2026-03-20 15:52:35 -0300

    teste

--- htdocs/install/install.forced.php
+++ htdocs/install/install.forced.php
@@ -1,3 +1,2 @@
 <?php
-/** @var string $force_install_module				Enable module(s) (Comma separated class names list, example: modSociete,modFournisseur,modFacture) */
-$force_install_module = 'modFacture, modSociete, modLabApp, modNFSe, modNFe';
+$force_install_module = 'modFacture, modLabApp, modNFSe, modNFe';
