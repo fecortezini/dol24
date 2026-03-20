561defdfe526a53c077f4fe1d3172374e696b5cf
felipe cortezini <felipemasterx21@gmail.com>
2026-03-20 17:01:08 -0300

    test

--- htdocs/install/install.forced.php
+++ htdocs/install/install.forced.php
@@ -1,3 +1,3 @@
 <?php
 $force_install_noedit = 2;
-$force_install_module = 'modLabApp,modNFSe,modNFe,modFacture';
+$force_install_module = 'modLabApp,modNFSe,modNFe,modFacture,modSociete,modProduct,modService';
